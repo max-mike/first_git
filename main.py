408fa8a3f6a71db6816d572d741c07a27905efc9
Andrey Maximov <andrey_m2@Andreys-MacBook-Pro.local>
2023-10-15 22:12:52 +0300

    Telegram Bot feature

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+from dotenv import load_dotenv
+import os
+import asyncio
+import logging
 
-def main():
-    # request of user name
-    name = input()
-    # print hello
-    print(f"Hello, {name}!")
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from handlers import router
+
+
+async def main():
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Get the bot token from the environment variable
+    bot_token = os.getenv("BOT_TOKEN")
+
+    bot = Bot(token=bot_token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
